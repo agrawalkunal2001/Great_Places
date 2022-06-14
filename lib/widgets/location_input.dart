0fbe3dfd9ced5a4fafb845a8bbbbf04b774b3ecc
Plane <plane2001@outlook.com>
2022-06-14 12:52:58 +0530

    Displayed user's current location on map

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/helpers/location_helper.dart';
 import 'dart:io';
 import 'package:location/location.dart';
 import 'package:path/path.dart' as path;
@@ -16,8 +17,11 @@ class _LocationInputState extends State<LocationInput> {
 
   Future<void> _getUserCurrentLocation() async {
     final locationData = await Location().getLocation();
-    print(locationData.latitude);
-    print(locationData.longitude);
+    final staticMapImageUrl = LocationHelper.generateLocationPreviewImage(
+        latitude: locationData.latitude, longitude: locationData.longitude);
+    setState(() {
+      _previewImageUrl = staticMapImageUrl;
+    });
   }
 
   @override
